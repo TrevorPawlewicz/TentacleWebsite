dc7f7ec0422c69e2797756d5801b970b2b6ed1d5
Trev <trevor.pawlewicz@gmail.com>
2015-09-28 14:57:03 -0700

    inital website files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// height of img to height of the window:
+$(".contentContainer").css("min-height", $(window).height());
+
+
+$("#ionicBtn").click(function(){
+    window.location = 'http://ionicframework.com/';
+});
